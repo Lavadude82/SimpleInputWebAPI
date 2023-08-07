5bd91da1c2c4710b68e0718e51128c3e37290d91
Lavadude82 <irvinduarte2009@gmail.com>
2023-08-07 14:55:34 -0500

    Added gamepad.on and gi to g.i

--- inputApi.js
+++ inputApi.js
@@ -3,7 +3,72 @@
 #USING MIT LICENSE
 */
 
-var mouse = {
+const gamepad = {
+  on: function (event, callback, index) {
+    if (event == "button") {
+      setInterval(() => {
+        if (index == undefined) {
+          for (let i = 0; i < 3; i++) {
+            const btn = gamepad.input(i).buttons;
+            Object.keys(btn).forEach((e, i) => {
+              Object.values(btn)[i] == true;
+              callback({ index: i, button: e });
+            });
+          }
+        } else {
+          const btn = gamepad.input(index).buttons;
+          Object.keys(btn).forEach((e,i)=>{
+            if(Object.values(btn)[i]){
+              callback({index:index,button:e})
+            }
+          })
+        }
+      });
+    }
+  },
+  input: function (index) {
+    if (index < 0 || index > 4) {
+      return null;
+    }
+    let gamepad = navigator.getGamepads()[index];
+    if (gamepad == null) return null;
+    let GamepadData = {
+      joysticks: {
+        left: {
+          x: gamepad.axes[0],
+          y: gamepad.axes[1],
+          down: gamepad.buttons[10].pressed,
+        },
+        right: {
+          x: gamepad.axes[2],
+          y: gamepad.axes[3],
+          down: gamepad.buttons[11].pressed,
+        },
+      },
+      buttons: {
+        A: gamepad.buttons[0].pressed,
+        B: gamepad.buttons[1].pressed,
+        X: gamepad.buttons[2].pressed,
+        Y: gamepad.buttons[3].pressed,
+        start: gamepad.buttons[9].pressed,
+        select: gamepad.buttons[8].pressed,
+        home: gamepad.buttons[16].pressed,
+        LT: gamepad.buttons[6].pressed,
+        RT: gamepad.buttons[7].pressed,
+        LB: gamepad.buttons[4].pressed,
+        RB: gamepad.buttons[5].pressed,
+      },
+      dpad: {
+        up: gamepad.buttons[12].pressed,
+        down: gamepad.buttons[13].pressed,
+        left: gamepad.buttons[14].pressed,
+        right: gamepad.buttons[15].pressed,
+      },
+    };
+    return GamepadData;
+  },
+};
+let mouse = {
   x: 0,
   y: 0,
   movx: 0,
@@ -15,7 +80,7 @@ var mouse = {
   middle: false,
   scroll: 0,
 };
-var keys = {};
+let keys = {};
 onkeyup = onkeydown = function (e) {
   keys[e.keyCode] = e.type == "keydown";
 };
@@ -40,49 +105,9 @@ onmousedown = onmouseup = function (e) {
   mouse.middle = e.button == 1 && e.type == "mousedown";
   mouse.right = e.button == 2 && e.type == "mousedown";
 };
-function gamepadInput(index) {
-  if (index < 0 || index > 4) {
-    return null;
-  }
-  var gamepad = navigator.getGamepads()[index];
-  if (gamepad == null) return null;
-  var GamepadData = {
-    joysticks: {
-      left: {
-        x: gamepad.axes[0],
-        y: gamepad.axes[1],
-        down: gamepad.buttons[10].pressed,
-      },
-      right: {
-        x: gamepad.axes[2],
-        y: gamepad.axes[3],
-        down: gamepad.buttons[11].pressed,
-      },
-    },
-    buttons: {
-      A: gamepad.buttons[0].pressed,
-      B: gamepad.buttons[1].pressed,
-      X: gamepad.buttons[2].pressed,
-      Y: gamepad.buttons[3].pressed,
-      start: gamepad.buttons[9].pressed,
-      select: gamepad.buttons[8].pressed,
-      home: gamepad.buttons[16].pressed,
-      LT: gamepad.buttons[6].pressed,
-      RT: gamepad.buttons[7].pressed,
-      LB: gamepad.buttons[4].pressed,
-      RB: gamepad.buttons[5].pressed,
-    },
-    dpad: {
-      up: gamepad.buttons[12].pressed,
-      down: gamepad.buttons[13].pressed,
-      left: gamepad.buttons[14].pressed,
-      right: gamepad.buttons[15].pressed,
-    },
-  };
-  return GamepadData;
-}
+
 function mouseInput() {
-  var mouseData = {
+  let mouseData = {
     x: mouse.x,
     y: mouse.y,
     buttons: {
@@ -108,4 +133,5 @@ function mouseInput() {
 function getKey(keycode) {
   return keys[keycode] || false;
 }
-export { gamepadInput, mouseInput, getKey };
+
+export { gamepad, mouseInput, getKey };
